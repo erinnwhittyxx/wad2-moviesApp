1060d566d0a2bb2c23022a6e5169982827d95270
erinnwhittyxx <erinnwhittyxx@gmail.com>
2020-11-04 11:11:27 +0000

    Refactor: Movie list and genre list now stored in contexts

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage' 
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
 
 const App = () => {
   return (
     <BrowserRouter>
     <div className="jumbotron">
-      <SiteHeader />      {/* New Header  */}
+      <SiteHeader />   
       <div className="container-fluid">
         <ul className="navbar-nav text-black">
           <li className="nav-item">
@@ -31,13 +33,17 @@ const App = () => {
             </Link>
           </li>
         </ul>
-        <Switch>
-          <Route path="/reviews/:id" component={MovieReviewPage} />
-          <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-          <Route path="/movies/:id" component={MoviePage} />
-          <Route path="/" component={HomePage} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <MoviesContextProvider>
+          <GenresContextProvider> 
+            <Switch>
+              <Route path="/reviews/:id" component={MovieReviewPage} />
+              <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+              <Route path="/movies/:id" component={MoviePage} />
+              <Route path="/" component={HomePage} />
+              <Redirect from="*" to="/" />
+            </Switch>
+          </GenresContextProvider> 
+        </MoviesContextProvider>
       </div>
     </div>
   </BrowserRouter>
